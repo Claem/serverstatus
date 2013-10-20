f0dadab67ac7659966e9bf367e870feacbc175b3
Claem <magdaguirre@gmail.com>
2013-10-19 17:22:24 -0700

    BODY READ
    All the info needed/required is in the BODY, script now displays only
    that.  Next: Having BS parse and remove HTML language, tags, etc.

--- riftserver.py
+++ riftserver.py
@@ -5,4 +5,6 @@ from bs4 import BeautifulSoup
 site = urllib2.urlopen('http://www.riftgame.com/en/shardstatus/')
 status = site.read()
 soup = BeautifulSoup(status)
-print soup
+#text = findAll(text=True)
+
+print soup.body
