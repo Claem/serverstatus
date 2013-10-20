b9642e7fb210962552558ae4108aa7a3f31e23b4
Claem <magdaguirre@gmail.com>
2013-10-19 17:36:49 -0700

    Filtering
    More filtering of info.  Removed a majority of tags, and defined it.
    Next : Even tighter filtering.

--- riftserver.py
+++ riftserver.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 site = urllib2.urlopen('http://www.riftgame.com/en/shardstatus/')
 status = site.read()
 soup = BeautifulSoup(status)
-#text = findAll(text=True)
+text = soup.body.find_all(text=True)
 
-print soup.body
+print text
+
