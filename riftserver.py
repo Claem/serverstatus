c5871e63d5732fe174ed82b9dd97a262bb915935
Claem <magdaguirre@gmail.com>
2013-10-24 14:59:17 -0700

    identifies and prints shards.  More filtering.

--- riftserver.py
+++ riftserver.py
@@ -4,6 +4,14 @@ from xml.dom import minidom
 
 site = urllib2.urlopen('http://status.riftgame.com/na-status.xml')
 status = site.read()
+xmldoc = minidom.parseString(status)
+itemlist = xmldoc.getElementsByTagName('shard')
 
-print status 
+print len(itemlist)
+print itemlist[0].attributes['name'].value
+for s in itemlist :
+    print s.attributes['name'].value
+
+
+
 
