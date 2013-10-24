8252673a43727325d0660e0bbffa2d49d9c535e7
Claem <magdaguirre@gmail.com>
2013-10-24 11:29:20 -0700

    Minidom, Go!!!
    Trashed most of old code.  Using minidom instead, making sure program
    can pull and display wanted xml correctly.

--- riftserver.py
+++ riftserver.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python 
 import urllib2
-from bs4 import BeautifulSoup
+from xml.dom import minidom
 
 site = urllib2.urlopen('http://status.riftgame.com/na-status.xml')
 status = site.read()
-soup = BeautifulSoup(status)
 
-#Retreive the first table, returning the data we're looking for. 
-#Needs more filtering.
-table = soup.find("table")
-
-rows=list()
-for row in table.findAll("tr"):
-   rows.append(row)
-
-for row in rows:
-		print row
+print status 
 
