37103b443c42992aab115521db015d1e1c8d8067
Claem <magdaguirre@gmail.com>
2013-10-24 16:41:49 -0700

    online stat working
    now we have working functions that can call on the server statusf of
    faeblight.  Now to ask the program to check specific servers.

--- riftserver.py
+++ riftserver.py
@@ -2,16 +2,15 @@
 import urllib2
 from xml.dom import minidom
 
-site = urllib2.urlopen('http://status.riftgame.com/na-status.xml')
-status = site.read()
-xmldoc = minidom.parseString(status)
-itemlist = xmldoc.getElementsByTagName('shard')
+def onlinestat(server):
+    site = urllib2.urlopen('http://status.riftgame.com/na-status.xml')
+    status = site.read()
+    xmldoc = minidom.parseString(status)
+    itemlist = xmldoc.getElementsByTagName('shard')
+    for s in itemlist:
+        if s.attributes['name'].value == server:
+            return s.attributes['online'].value
+            
 
-for s in itemlist :
-    if s.attributes['name'].value == 'Faeblight' :
-      #  print s.attributes['online'].value
-        if s.attributes['online'].value == 'True'  :
-            print 'Go Play!'
-
-
-
+if onlinestat('Faeblight') == 'True':
+    print 'Go Play!'
