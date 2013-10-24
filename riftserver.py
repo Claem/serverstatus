c28e10f9ffbd43989e690933569a80e4f65f5597
Claem <magdaguirre@gmail.com>
2013-10-24 00:55:09 -0700

    Screw HTML!!
    Found XML link giving only required info.  Will dump this current code
    to try XML.

--- riftserver.py
+++ riftserver.py
@@ -2,10 +2,18 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-site = urllib2.urlopen('http://www.riftgame.com/en/shardstatus/')
+site = urllib2.urlopen('http://status.riftgame.com/na-status.xml')
 status = site.read()
 soup = BeautifulSoup(status)
-text = soup.body.find_all(text=True)
 
-print text
+#Retreive the first table, returning the data we're looking for. 
+#Needs more filtering.
+table = soup.find("table")
+
+rows=list()
+for row in table.findAll("tr"):
+   rows.append(row)
+
+for row in rows:
+		print row
 
